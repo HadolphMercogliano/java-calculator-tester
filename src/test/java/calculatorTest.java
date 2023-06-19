60e6f0fee76346672cb416296649b6a11b3d3a9e
HadolphMercogliano <giovanni.mercogliano@gmail.com>
2023-06-19 17:18:58 +0200

    add tests

--- src/test/java/calculatorTest.java
+++ src/test/java/calculatorTest.java
@@ -1,11 +1,46 @@
 import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 class calculatorTest {
   
   private Calculator calc;
   
   @BeforeEach
   void init() {
-    calc = new Calculator(); //creo una nuova istanza della classe
+    calc = new Calculator();
   }
   
+  @Test
+  @DisplayName("Test calculator sum")
+  void testSum() {
+    assertEquals(3, calc.sum(1.0f, 2.0f));
+  }
+  
+  @Test
+  @DisplayName("Test calculator subtraction")
+  
+  void testSubtract() {
+    assertEquals(  1.0f, calc.subtract(3.0f, 2.0f));
+  }
+  
+  @Test
+  @DisplayName("Test calculator multiplication")
+  void testMultiply() {
+    assertEquals(1.0f, calc.multiply(1.0f,1.0f));
+  }
+  
+  @Test
+  @DisplayName("Test calculator division")
+  void testDivide() {
+    assertEquals(2.0f, calc.divide(4.0f, 2.0f));
+  }
+  @Test
+  @DisplayName("Test calculator division error case ")
+  void divideException() {
+    assertThrows(
+      IllegalArgumentException.class, () -> calc.divide(3.0f, -2.0f)
+    );
+  }
 }
