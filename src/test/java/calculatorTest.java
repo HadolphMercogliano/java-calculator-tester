8aa9feb47bf393ef7a107677e259298a67527e67
HadolphMercogliano <giovanni.mercogliano@gmail.com>
2023-06-19 17:19:25 +0200

    add tests

--- src/test/java/calculatorTest.java
+++ src/test/java/calculatorTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-class calculatorTest {
+class CalculatorTest {
   
   private Calculator calc;
   
